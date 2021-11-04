f83b60f05c75e5cf3f0b69bf16c079dd07bcce1a
Victor Caruso <vic.caruso@gmail.com>
2021-11-04 15:23:02 -0700

    added keydown events and sound counters

--- app.js
+++ app.js
@@ -7,22 +7,50 @@ const dogSound = document.getElementById('dog-sound');
 const catSound = document.getElementById('cat-sound');
 const horseSound = document.getElementById('horse-sound');
 
+const clickCount = document.getElementById('click-count');
+
 // initialize global state
 
+let woofCount = 0;
+let meowCount = 0;
+let neighCount = 0;
 
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
-dogCard.addEventListener('click', ()=> {
+// click events
+dogCard.addEventListener('click', () => {
   dogSound.play();
+  woofCount++;
+  clickCount.textContent = `Woofs: ${woofCount}`;
 });
 
-catCard.addEventListener('click', ()=> {
+catCard.addEventListener('click', () => {
   catSound.play();
+  meowCount++;
+  clickCount.textContent = `Meows: ${meowCount}`;
 });
 
-horseCard.addEventListener('click', ()=> {
+horseCard.addEventListener('click', () => {
   horseSound.play();
+  neighCount++;
+  clickCount.textContent = `Neighs: ${neighCount}`;
+});
+
+// Keydown events
+document.addEventListener('keydown', (e) => {
+
+  if (e.key === 'd' || e.key === 'D') {
+    dogSound.play();
+    woofCount++;
+    clickCount.textContent = `Woofs: ${woofCount}`;
+
+  } else if (e.key === 'c' || e.key === 'C') {
+    catSound.play();
+    meowCount++;
+    clickCount.textContent = `Meows: ${meowCount}`;
+
+  } else if (e.key === 'h' || e.key === 'H') {
+    horseSound.play();
+    neighCount++;
+    clickCount.textContent = `Neighs: ${neighCount}`;
+
+  }
 });
